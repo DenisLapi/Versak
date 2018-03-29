cbc883ebf8ce5271d7bdaff394ea83e3c3f5b897
DenisLapi <denislapidenis@gmail.com>
2018-03-29 21:57:22 +0300

    Navigation and hero section finished

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+// Responsive menu
+
+$(".menu-btn").on("click", function() {
+	var navigation = $(".right-side ul");
+	if(!navigation.attr("nav-active")) {
+		navigation.animate({ height: 200 }, 500);
+		navigation.attr("nav-active","true");
+	} else {
+		navigation.animate({ height: 0 }, 500);
+		navigation.removeAttr("nav-active");
+	}
+});
+
